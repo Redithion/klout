d91eda283a1b0c48bd2c0911d7827074051649b4
Redithion <Redithion@users.noreply.github.com>
2017-07-25 17:39:55 -0300

    Replace HTTParty with mechanize
    Replaced HTTParty with Mechanize as the first one was having issues with the Json response

--- lib/klout.rb
+++ lib/klout.rb
@@ -1,6 +1,6 @@
 require 'cgi'
 require 'uri'
-require 'httparty'
+require 'mechanize'
 require 'hashie'
 Hash.send :include, Hashie::Extensions
 
@@ -26,7 +26,7 @@ module Klout
     # Allows the initializer to turn off actually communicating to the REST service for certain environments
     # Requires fakeweb gem to be installed
     def disable
-      FakeWeb.register_uri(:any, %r|#{Regexp.escape(Klout.base_uri)}|, :body => '{"Disabled":true}', :content_type => 'application/json; charset=utf-8')
+      FakeWeb.register_uri(:any, %r|#{Regexp.escape(Klout.base_uri)}|, body: '{"Disabled":true}', content_type: 'application/json; charset=utf-8')
     end
   end
 
@@ -47,34 +47,59 @@ module Klout
   class Unavailable < StandardError; end
 
   class Klout
-    include HTTParty
 
-    @@base_uri = "http://api.klout.com/v2/"
-    @@api_key = ""
-    headers({ 
+    @base_uri = "http://api.klout.com/v2/"
+    @api_key = ""
+    @headers = {
       'User-Agent' => "klout-rest-#{VERSION}",
       'Content-Type' => 'application/json; charset=utf-8',
-      'Accept-Encoding' => 'gzip, deflate'
-    })
-    base_uri @@base_uri
+      'Accept-Encoding' => 'gzip, deflate',
+      'Accept' => 'application/json'
+    }
+
+    @agent = Mechanize.new
+    @agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     class << self
       # Get the API key
-      def api_key; @@api_key end
-      
+      def api_key
+        @api_key
+      end
+
       # Set the API key
       def api_key=(api_key)
-        return @@api_key unless api_key
-        @@api_key = api_key
+        return @api_key unless api_key
+        @api_key = api_key
       end
 
       # Get the Base URI.
-      def base_uri; @@base_uri end
+      def base_uri
+        @base_uri
+      end
 
-      def get(*args); handle_response super end
-      def post(*args); handle_response super end
-      def put(*args); handle_response super end
-      def delete(*args); handle_response super end
+      def get(*args)
+        path, options, = args
+        url = get_absolute_url @base_uri, path
+        handle_response @agent.get(url, options[:query], nil, @headers)
+      end
+
+      def post(*args)
+        path, options, = args
+        url = get_absolute_url @base_uri, path
+        handle_response @agent.post(url, options[:query], nil, @headers)
+      end
+
+      def put(*args)
+        path, options, = args
+        url = get_absolute_url @base_uri, path
+        handle_response @agent.put(url, options[:query], nil, @headers)
+      end
+
+      def delete(*args)
+        path, options, = args
+        url = get_absolute_url @base_uri, path
+        handle_response @agent.delete(url, options[:query], nil, @headers)
+      end
 
       def handle_response(response) # :nodoc:
         case response.code
@@ -89,9 +114,20 @@ module Klout
         when 500...600
           raise ServerError.new
         else
-          response
+          JSON.parse response.body
+        end
+      end
+
+      def get_absolute_url(parent_url, link_url)
+        return nil if link_url.nil?
+        begin
+          return URI.join(parent_url, link_url).to_s
+        rescue URI::InvalidURIError => e
+          raise e if url_encoded? link_url
+          return URI.join(parent_url, URI.encode(link_url)).to_s
         end
       end
     end
   end
 end
+
