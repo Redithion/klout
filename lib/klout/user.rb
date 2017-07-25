82eb09b71b218f6d591f6bc3d30cf217bff6194e
Redithion <Redithion@users.noreply.github.com>
2017-07-25 17:41:08 -0300

    Adapted users to Mechanize

--- lib/klout/user.rb
+++ lib/klout/user.rb
@@ -5,40 +5,39 @@ module Klout
   # Represents a user
   class User
     attr_reader :klout_id
-    
+
     def initialize(klout_id)
       @klout_id = klout_id
     end
-    
+
     def details
       response = get
       Hashie::Mash.new(response)
     end
-    
+
     def score
-      response = get "score"
+      response = get "/score"
       Hashie::Mash.new(response)
     end
-    
+
     def topics
-      response = get "topics"
-      response.parsed_response
+      response = get "/topics"
+      response
     end
-    
+
     def influence
-      response = get "influence"
+      response = get "/influence"
       Hashie::Mash.new(response)
     end
-    
+
     private
-    
+
     def get(action = nil)
       Klout.get uri_for(action), :query => {key: Klout.api_key}
     end
-    
+
     def uri_for(action = nil)
-      action = "/#{action}" if action
-      "/user.json/#{klout_id}#{action}"
+      "user.json/#{klout_id}#{action}"
     end
   end
 end
